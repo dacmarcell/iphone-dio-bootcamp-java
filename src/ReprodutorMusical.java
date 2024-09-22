f1c83e724004ae4fbb7940de4bceb6f1c374c7c7
dacmarcell <83461178+dacmarcell@users.noreply.github.com>
2024-09-21 23:33:45 -0300

    feat: cria reprodutor musical

---
+++ src/ReprodutorMusical.java
@@ -0,0 +1,13 @@
+public class ReprodutorMusical extends Iphone {
+  public void tocar() {
+    System.out.println("Tocando...");
+  }
+
+  public void pausar() {
+    System.out.println("Pausando...");
+  }
+
+  public void selecionarMusica() {
+    System.out.println("Selecionando música...");
+  }
+}
